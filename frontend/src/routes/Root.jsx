90678af4c4794f343cc3663e154efb48001ad07b
Grantego <grantego@gmail.com>
2024-05-01 14:56:31 -0500

    completed landing page

--- frontend/src/routes/Root.jsx
+++ frontend/src/routes/Root.jsx
@@ -6,7 +6,9 @@ const Root = () => {
  return (
     <>
         <NavBar/>
-        <h1>test</h1>
+        {localStorage.getItem('token') ?
+        <h1>{`Welcome back, ${localStorage.getItem('username')}`}</h1> :
+        <h1>Welcome to Jobly!</h1>}
     </>
  )
 }
